b6660e7aadf60ab5557bb02d77e6ded9b0eb0eb9
hyeongsock <ggomarain1302@gmail.com>
2023-10-27 16:46:21 +0900

    Feat: "데이터 저장 기능 추가"

--- CookApss_CodingTest/Assets/01.Scripts/Managers/DataManager.cs
+++ CookApss_CodingTest/Assets/01.Scripts/Managers/DataManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +7,58 @@ using UnityEngine;
 public class DataManager
 {
     public Dictionary<int, DialogData> dialogDatas;
+    public readonly string PLAYERSAVEDATA_PATH = Path.Combine(Application.dataPath + "/04.Datas/", "PlayerSaveData.txt");
+
     public T Get<T>(int _UID) where T : Data
     {
+        if (typeof(T) == typeof(PlayerData)) return GetPlayerData(_UID) as T;
         if (typeof(T) == typeof(DialogData)) if (dialogDatas.TryGetValue(_UID, out DialogData _data)) return _data as T;
 
         Debug.LogError("데이터가 반환되지 않았습니다.");
         return null;
+    }
+
+    private PlayerData GetPlayerData(int _UID)
+    {
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>("PlayerSaveData");
+        PlayerSaveData saveData = JsonUtility.FromJson<PlayerSaveData>(textAsset.text);
+
+        string[] entityUIDs = saveData.hasEntityUID.Split(',');
+        string[] entityLevels = saveData.hasEntityLevel.Split(',');
+
+        List<BaseBattleEntityData> entityDatas = new List<BaseBattleEntityData>();
+
+        for (int i = 0; i < entityUIDs.Length; i++)
+        {
+            BaseBattleEntityData entityData = new BaseBattleEntityData(Int32.Parse(entityUIDs[i]), Int32.Parse(entityLevels[i]));
+            entityDatas.Add(entityData);
+        }
+
+        PlayerData playerData = new PlayerData(saveData.UID, saveData.name, entityDatas);
+        return playerData;
+    }
+
+    public void SavePlayerData(PlayerData _playerData)
+    {
+        string hasEntityUIDs = string.Empty;
+        string hasEntityLevel = string.Empty;
+
+        for (int i = 0; i < _playerData.hasEntites.Count; i++)
+        {
+            if(i == _playerData.hasEntites.Count-1)
+            {
+                hasEntityUIDs += (_playerData.hasEntites[i].UID);
+                hasEntityLevel += (_playerData.hasEntites[i].level);
+                continue;
+            }
+            hasEntityUIDs += (_playerData.hasEntites[i].UID + ",");
+            hasEntityLevel += (_playerData.hasEntites[i].level + ",");
+        }
+
+        PlayerSaveData saveData = new PlayerSaveData(_playerData.UID,_playerData.name, hasEntityUIDs, hasEntityLevel);
+        string saveDataJson = JsonUtility.ToJson(saveData);
+
+        File.WriteAllText(PLAYERSAVEDATA_PATH, saveDataJson);
     }
 
     public DataManager()
@@ -19,10 +67,58 @@ public class DataManager
     }
 }
 
+
+
 public class Data
 {
 
 }
+
+[System.Serializable]
+public class PlayerData : Data
+{
+    public int UID;
+    public string name;
+    public List<BaseBattleEntityData> hasEntites;
+
+    public PlayerData(int _UID, string _name, List<BaseBattleEntityData> _hasEntites)
+    {
+        UID = _UID;
+        name = _name;
+        hasEntites = _hasEntites;
+    }
+}
+
+[System.Serializable]
+public class PlayerSaveData : Data
+{
+    public int UID;
+    public string name;
+    public string hasEntityUID;
+    public string hasEntityLevel;
+
+    public PlayerSaveData(int _UID, string _name, string _hasEntityUID, string _hasEntityLevel)
+    {
+        UID = _UID;
+        name = _name;
+        hasEntityUID = _hasEntityUID;
+        hasEntityLevel = _hasEntityLevel;
+    }
+}
+
+[System.Serializable]
+public class BaseBattleEntityData
+{
+    public int UID;
+    public int level;
+    
+    public BaseBattleEntityData(int _UID, int _level)
+    {
+        UID = _UID;
+        level = _level;
+    }
+}
+
 
 [System.Serializable]
 public class DialogData : Data
--- CookApss_CodingTest/Assets/01.Scripts/Managers/GameManager.cs
+++ CookApss_CodingTest/Assets/01.Scripts/Managers/GameManager.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Define;
 
 public class GameManager : Singleton<GameManager>
 {
+    public GameState state;
+    public PlayerData data;
 
+    public void Awake()
+    {
+        Managers.Resource.LoadAllAsync<UnityEngine.Object>("Preload", _completeCallback: () => 
+        { 
+            StartGame();
+        });
+    }
+    
+    public void StartGame()
+    {
+        data = Managers.Data.Get<PlayerData>(0);
+    }
+
+    public void SaveGame()
+    {
+        Managers.Data.SavePlayerData(data);
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        if (pause)
+            SaveGame();
+    }
 }
+
+
